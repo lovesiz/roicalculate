23691d71eee9c87c4811c7a3021c536195cc55bc
lovesiz <115664111+lovesiz@users.noreply.github.com>
2022-10-15 00:17:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ let btn = document.getElementById("btn");
 btn.onclick = function () {
   let mp = parseFloat(document.getElementById("mp").value);
   let np = parseFloat(document.getElementById("np").value);
+  let mb = parseFloat(document.getElementById("mb").value);
+  let mp2 = mp + (mp * mb) / 100;
   let br = 20;
-  let mp1 = mp * 1000000000;
+  let mp1 = mp2 * 1000000000000;
   let np1 = np * 1000000000000000000;
   let price = parseFloat(document.getElementById("price").value);
 
@@ -13,5 +15,5 @@ btn.onclick = function () {
   let s = document.getElementById("di");
   s.innerHTML = daily.toFixed(8);
   let mrd = document.getElementById("mrd");
-  mrd.innerHTML = price / daily + "days";
+  mrd.innerHTML = price / daily + " gün";
 };
